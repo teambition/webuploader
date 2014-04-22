fc4cf49e23cc4d7e531d9892cc90cb6ecbe6f7ed
Yan Qing <admin@zensh.com>
2014-04-22 10:47:31 +0800

    fix for file picker

--- dist/webuploader.custom.js
+++ dist/webuploader.custom.js
@@ -923,8 +923,8 @@
                     position: 'absolute',
                     top: '0px',
                     left: '0px',
-                    width: '1px',
-                    height: '1px',
+                    width: '100%',
+                    height: '100%',
                     overflow: 'hidden'
                 });
     
@@ -3701,6 +3701,7 @@
             }
         });
     });
+    
     /**
      * Terms:
      *
